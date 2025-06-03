7a1e35056d093e43ec6f2080b85615ad33b2d445
Christian Brandon Neri Sanchez <cbneris@gmail.com>
2025-06-03 17:26:22 -0600

    Se agrega el metodo para buscar por el campo 'login'

--- src/main/java/com/cbns/adea/usuario/repository/UsuarioRepository.java
+++ src/main/java/com/cbns/adea/usuario/repository/UsuarioRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.cbns.adea.usuario.entity.UsuarioEntity;
 
 public interface UsuarioRepository extends JpaRepository<UsuarioEntity, String>{
-	
+	UsuarioEntity findByLogin(String login);
 }
