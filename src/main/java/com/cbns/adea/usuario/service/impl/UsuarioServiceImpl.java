60c05f72438313674748a96cbc5e87237e32b0a9
Christian Brandon Neri Sanchez <cbneris@gmail.com>
2025-06-03 22:50:50 -0600

    Se agrega la herencia del commonServiceImpl para heredar los metodos de consulta

--- src/main/java/com/cbns/adea/usuario/service/impl/UsuarioServiceImpl.java
+++ src/main/java/com/cbns/adea/usuario/service/impl/UsuarioServiceImpl.java
@@ -1,7 +1,13 @@
 package com.cbns.adea.usuario.service.impl;
 
+import org.springframework.stereotype.Service;
+
+import com.cbns.adea.usuario.entity.UsuarioEntity;
+import com.cbns.adea.usuario.repository.UsuarioRepository;
 import com.cbns.adea.usuario.service.UsuarioService;
+import com.dervmark.commons.service.impl.CommonServiceImpl;
 
-public class UsuarioServiceImpl implements UsuarioService{
-
+@Service
+public class UsuarioServiceImpl extends CommonServiceImpl<UsuarioEntity, UsuarioRepository> implements UsuarioService{
+	
 }
