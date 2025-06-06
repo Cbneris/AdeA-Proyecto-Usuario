308523e10eafd8f3a1ccd3e4a4b7437767f19ecb
Christian Brandon Neri Sanchez <cbneris@gmail.com>
2025-06-06 17:21:15 -0600

    Se agregan los campos del usuario respone

--- src/main/java/com/cbns/adea/usuario/dto/UsuarioResponse.java
+++ src/main/java/com/cbns/adea/usuario/dto/UsuarioResponse.java
@@ -1,5 +1,32 @@
 package com.cbns.adea.usuario.dto;
 
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
 public class UsuarioResponse {
 	
+	private String 	login;
+    private String 	nombre;
+    private Double 	cliente;
+    private String 	email;
+    private Date 	fechaAlta;
+    private Date 	fechaBaja;
+    private char 	status;
+    private Double 	intentos;
+    private Date 	fechaRevocado;
+    private Date 	fechaVigencia;
+    private Integer noAcceso;
+    private String 	apellidoPaterno;
+    private String 	apellidoMaterno;
+    private int 	area;
+    private Date 	fechaModificacion;
+    
 }
