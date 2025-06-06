4b16b1757ec84213d4e76bb27ef8d9aaeed077c2
Christian Brandon Neri Sanchez <cbneris@gmail.com>
2025-06-06 10:37:54 -0600

    Se agrega la herencia del controlador base

--- src/main/java/com/cbns/adea/usuario/controller/UsuarioController.java
+++ src/main/java/com/cbns/adea/usuario/controller/UsuarioController.java
@@ -3,8 +3,12 @@ package com.cbns.adea.usuario.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cbns.adea.usuario.entity.UsuarioEntity;
+import com.cbns.adea.usuario.service.UsuarioService;
+import com.dervmark.commons.controller.CommonController;
+
 @RestController
 @RequestMapping("/usuarios")
-public class UsuarioController {
-
+public class UsuarioController extends CommonController<UsuarioEntity, UsuarioService>{
+	
 }
