8b8d33fc53d70bc9dba311b524bf7ae5d0ff99fc
Christian Brandon Neri Sanchez <cbneris@gmail.com>
2025-06-08 20:38:50 -0600

    Refactorización de los metodos y librerias importadas

--- src/main/java/com/cbns/adea/usuario/service/LoginService.java
+++ src/main/java/com/cbns/adea/usuario/service/LoginService.java
@@ -3,8 +3,7 @@ package com.cbns.adea.usuario.service;
 import java.util.Optional;
 
 import com.cbns.adea.usuario.entity.UsuarioEntity;
-import com.dervmark.commons.service.CommonService;
 
-public interface LoginService extends CommonService<UsuarioEntity>{
+public interface LoginService{
 	Optional<UsuarioEntity> buscaUsuarioPorLogin(String login);
 }
--- src/main/java/com/cbns/adea/usuario/service/impl/LoginServiceImpl.java
+++ src/main/java/com/cbns/adea/usuario/service/impl/LoginServiceImpl.java
@@ -2,19 +2,22 @@ package com.cbns.adea.usuario.service.impl;
 
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.cbns.adea.usuario.entity.UsuarioEntity;
 import com.cbns.adea.usuario.repository.UsuarioRepository;
 import com.cbns.adea.usuario.service.LoginService;
-import com.dervmark.commons.service.impl.CommonServiceImpl;
 
 @Service
-public class LoginServiceImpl extends CommonServiceImpl<UsuarioEntity, UsuarioRepository> implements LoginService {
+public class LoginServiceImpl implements LoginService {
+	
+	@Autowired
+	UsuarioRepository usrRepo;
 
 	@Override
 	public Optional<UsuarioEntity> buscaUsuarioPorLogin(String login) {
-		return repository.findByLogin(login);
+		return usrRepo.findByLogin(login);
 	}
 	
 }
