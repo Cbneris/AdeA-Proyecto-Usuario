422a8ee3c94e86fa49d0ef33b01565a34a421295
Christian Brandon Neri Sanchez <cbneris@gmail.com>
2025-06-08 20:38:22 -0600

    Se creael metodo para buscar todos los usuarios y enviarlos al front omitiendo la contraseña

--- src/main/java/com/cbns/adea/usuario/controller/UsuarioController.java
+++ src/main/java/com/cbns/adea/usuario/controller/UsuarioController.java
@@ -1,31 +1,48 @@
 package com.cbns.adea.usuario.controller;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cbns.adea.usuario.commons.ResponseBuilder;
 import com.cbns.adea.usuario.dto.UsuarioDTO;
 import com.cbns.adea.usuario.entity.UsuarioEntity;
 import com.cbns.adea.usuario.mapper.UsuarioMapper;
 import com.cbns.adea.usuario.service.EncriptacionShaBase64;
 import com.cbns.adea.usuario.service.UsuarioService;
-import com.dervmark.commons.controller.CommonController;
-import com.dervmark.commons.response.ResponseBuilder;
 
 
 @RestController
 @RequestMapping("/usuarios")
-public class UsuarioController extends CommonController<UsuarioEntity, UsuarioService> {
+public class UsuarioController {
 
 	@Autowired
 	EncriptacionShaBase64 shaBase64;
+	
+	@Autowired
+	UsuarioService service;
+	
+	@GetMapping("/buscaAllUsers")
+	public ResponseEntity<?> buscarUsuarios() {
+		List<UsuarioDTO> listUsers = new ArrayList<>();
+		
+		for(UsuarioEntity usuario : service.findAll()) {
+			listUsers.add(UsuarioMapper.toDTO(usuario));
+		}		
+		
+		return ResponseBuilder.success("Lista obtenida correctamente", listUsers);
+	}
+	
 	
 	@PostMapping("/encriptionSave")
 	public ResponseEntity<?> guarda(@RequestBody UsuarioEntity entity){
