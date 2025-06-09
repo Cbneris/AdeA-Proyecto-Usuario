719c4b99aeeec0d9126084a30b14d6388185c78b
Christian Brandon Neri Sanchez <cbneris@gmail.com>
2025-06-08 20:39:03 -0600

    Se agregan los metodos service en la interfaz

--- src/main/java/com/cbns/adea/usuario/service/UsuarioService.java
+++ src/main/java/com/cbns/adea/usuario/service/UsuarioService.java
@@ -1,8 +1,17 @@
 package com.cbns.adea.usuario.service;
 
-import com.cbns.adea.usuario.entity.UsuarioEntity;
-import com.dervmark.commons.service.CommonService;
+import java.util.List;
+import java.util.Optional;
 
-public interface UsuarioService extends CommonService<UsuarioEntity>{
+import com.cbns.adea.usuario.entity.UsuarioEntity;
+
+public interface UsuarioService{
 	
+	public List<UsuarioEntity> findAll();
+	
+	public Optional<UsuarioEntity> findById(String id);
+	
+	public UsuarioEntity save(UsuarioEntity usuarioEntiy);
+	
+	public void deleteById(String id);
 }
