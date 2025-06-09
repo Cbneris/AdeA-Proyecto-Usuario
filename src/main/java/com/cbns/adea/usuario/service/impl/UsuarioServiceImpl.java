ba2573572addc7bf7c33973e1500e27d968784d7
Christian Brandon Neri Sanchez <cbneris@gmail.com>
2025-06-08 20:39:34 -0600

    Se implementan los servicios queson usados para el flujo de los usuarios

--- src/main/java/com/cbns/adea/usuario/service/impl/UsuarioServiceImpl.java
+++ src/main/java/com/cbns/adea/usuario/service/impl/UsuarioServiceImpl.java
@@ -1,13 +1,40 @@
 package com.cbns.adea.usuario.service.impl;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.cbns.adea.usuario.entity.UsuarioEntity;
 import com.cbns.adea.usuario.repository.UsuarioRepository;
 import com.cbns.adea.usuario.service.UsuarioService;
-import com.dervmark.commons.service.impl.CommonServiceImpl;
 
 @Service
-public class UsuarioServiceImpl extends CommonServiceImpl<UsuarioEntity, UsuarioRepository> implements UsuarioService{
+public class UsuarioServiceImpl implements UsuarioService{
+	
+	@Autowired
+	protected UsuarioRepository repository;
+
+	@Override
+	public List<UsuarioEntity> findAll() {
+		return repository.findAll();
+	}
+
+	@Override
+	public Optional<UsuarioEntity> findById(String id) {
+		return repository.findById(id);
+	}
+
+	@Override
+	public UsuarioEntity save(UsuarioEntity usuarioEntiy) {
+		return repository.save(usuarioEntiy);
+	}
+
+	@Override
+	public void deleteById(String id) {
+		repository.deleteById(id);
+		
+	}
 	
 }
